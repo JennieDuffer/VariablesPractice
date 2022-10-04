adf387a12d02b0ac3f3c0f3b4033f09e13d79a51
your-username <jenniej297@gmail.com>
2022-10-04 11:42:37 -0500

    initial commit

---
+++ VariablesPractice/Program.cs
@@ -0,0 +1,11 @@
+﻿string catName = "Ash";
+int catAge = 4;
+char nameLetter = 'A';
+bool isFluffy = true;
+double napsPerDay = 300;
+decimal catWeight = 11;
+
+Console.WriteLine($" My kitty's name is {catName}, she is {catAge} years old and weighs {catWeight}lbs. \n" +
+    $" Is she fluffy? The answer is {isFluffy}, she is such a fluffy cat. \n" +
+    $" She takes {napsPerDay} naps in a day!");
+
